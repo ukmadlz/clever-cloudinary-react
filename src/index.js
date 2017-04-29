bb8d55e5cbb646d547c06b810f4dbc7e74412d24
Mike Elsmore <mike@elsmore.me>
2017-04-29 22:00:29 +0100

    Now work with widths to automate the progressive tag

--- src/index.js
+++ src/index.js
@@ -2,36 +2,70 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { CloudinaryContext, Image, Transformation } from 'cloudinary-react';
 
 class CleverCloudinaryReact extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            // empty state, we might add something in future
-        };
+        // states
+        this.state= {
+          progressive: true
+        }
+        // defaults
+        this.progressiveThreshold = 200;
     }
 
     static get defaultProps() {
         return {
+          cloudName: 'demo',
+          publicId: '',
+          progressive: false,
+          privateCdn: '',
+          secureDistribution: '',
+          cname: '',
+          cdnSubdomain: ''
         }
     }
 
-    componentDidMount() {
+    componentWillMount() {
+      this.setState({
+        progressive: (this.props.progressive || (this.maxWidth() > this.progressiveThreshold))
+      });
     }
 
     componentDidUpdate(prevProps, prevState) {
     }
 
+    maxWidth() {
+      var widths = React.Children.map(
+        this.props.children,
+        function(child) {
+          return (parseInt(child.props.width)) ? parseInt(child.props.width) : 0;
+        }
+      );
+      return widths[widths.length-1];
+    }
+
     render() {
+
+        let progressiveTag = (this.state.progressive) ? <Transformation flags="progressive" /> : <Transformation flags="any_format" />;
+
         return(
-            <div className={ this.props.className }>
+            <CloudinaryContext
+              cloudName={ this.props.cloudName }
+              privateCdn={ this.props.privateCdn }
+              secureDistribution={ this.props.secureDistribution }
+              cname={ this.props.cname }
+              cdnSubdomain={ this.props.cdnSubdomain } >
+              <Image publicId={ this.props.publicId }>
+                { progressiveTag }
                 { this.props.children }
-            </div>
+              </Image>
+            </CloudinaryContext>
         )
     }
 
 }
 
-export default CleverCloudinaryReact;
+export { CleverCloudinaryReact, Transformation };
